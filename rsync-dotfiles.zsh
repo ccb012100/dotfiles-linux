52dca7f05daf14662def7c7ad22321f499957739
Christopher Bocardo <ccb012100@users.noreply.github.com>
2021-09-06 14:24:08 -0500

    add rsync file for syncing config files

---
+++ rsync-dotfiles.zsh
@@ -0,0 +1,54 @@
+# Sync config dotfiles to git repository
+# to run every day at 12:00,
+# run `crontab -e` and add the following line:
+# "0 12 * * * ~/.rsync-files.zsh"
+
+# -n : Dry run the command without transferring files
+# --list-only : Only show the list of files that rsync would transfer
+# -P : Show progress per file
+# -v : Show progress overall, outputting information about each file as it completes it
+# -u : Skip updating target files if they are newer than the source
+
+#########
+# $HOME #
+#########
+
+rsync -avuP --progress \
+      ~/.zshrc \
+      ~/.dotfiles/dotfiles/
+
+rsync -avuP --progress \
+      ~/.tmux.conf \
+      ~/.dotfiles/dotfiles/
+
+#################
+# $HOME/.config #
+#################
+
+rsync -avuP --progress \
+      ~/.config/bat/bat.conf \
+      ~/.dotfiles/dotfiles/.config/bat/
+
+rsync -avuP --progress \
+      ~/.config/gh/config.yml \
+      ~/.dotfiles/dotfiles/.config/gh/
+
+rsync -avuP --progress \
+      ~/.config/nvim/init.vim \
+      ~/.dotfiles/dotfiles/.config/nvim/
+
+##############
+# $HOME/.zsh #
+##############
+
+rsync -avuP --progress \
+      ~/.zsh/zsh-aliases \
+      ~/.dotfiles/dotfiles/.zsh/
+
+rsync -avuP --progress \
+      ~/.zsh/zsh-unalias \
+      ~/.dotfiles/dotfiles/.zsh/
+
+rsync -avuP --progress \
+      ~/.zsh/zsh-completions \
+      ~/.dotfiles/dotfiles/.zsh/
