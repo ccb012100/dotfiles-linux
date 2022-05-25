fd3a94679656ec611893e97b74886741f5431d74
Christopher Bocardo <ccb012100@users.noreply.github.com>
2022-05-25 13:20:28 -0500

    zsh: use functions instead of aliases

--- HOME/.zsh/aliases.zsh
+++ HOME/.zsh/aliases.zsh
@@ -17,44 +17,36 @@ alias llt='exa -l -t modified --sort newest'
 alias llat='exa -al -t modified --sort newest'
 
 ## built-in tools
-alias apt-up='sudo apt update && sudo apt upgrade'
+alias aptup='sudo apt update && sudo apt upgrade'
 alias cls='clear'                           # clear screen
 alias grep='grep --color=auto'
 alias lec='echo $?'                         # last exit code
 alias print-path='echo ${PATH//:/$"\n"}'    # print $PATH in human-readable format
 
 # external tools
-alias tree='exa --tree'
-alias sesh='tmux new-session -A -s sesh'
-alias ripgrep='rg'
-alias py='python3'
-alias powershell='pwsh'
-alias jq='jq -C'                            # colorize json
-alias gf='git forgit'
-alias gr='go run .'
-alias g='git'
-alias dot-r='dotnet run'
-alias dot-b='dotnet build'
-alias dc='docker compose'
-alias c.='code .'                           # open current directory in VS Code
 alias bat='batcat'
-alias pn='pnpm'
+alias c.='code .'                           # open current directory in VS Code
+alias dc='docker compose'
+alias g='git'
+alias gf='git forgit'
+alias jq='jq -C'                            # colorize json
 alias npm='echo "use pn/pnpm"'
+alias pn='pnpm'
+alias powershell='pwsh'
+alias py='python3'
+alias ripgrep='rg'
+alias sesh='tmux new-session -A -s sesh'
+alias tree='exa --tree'
 
 # .zshrc
 alias zedit="vim $HOME/.zshrc"
 alias zsource="source $HOME/.zshrc"
 
-# cargo (rust)
-alias cr='cargo run'
-alias cb='cargo build'
-alias cc='cargo check'
-
-# dotfiles configuration management
-alias cd-dotfiles='cd ~/.dotfiles'
-
 # directory aliases
 export ccb=~/ccb012100
+export dotfiles=~/.dotfiles
+export dotemacs=~/.emacs.d
+export zhell=~/.zsh
 
 # global aliases
 alias -g zf='| fzf' # pipe into fzf
--- HOME/.zsh/funcs.zsh
+++ HOME/.zsh/funcs.zsh
@@ -9,3 +9,56 @@ function clla { cd "$@" && ls -la }
 
 # open new emacs-frame
 function new-frame { emacsclient -e "(new-frame)" }
+
+# dotnet cli commands
+function dot() {
+    case $1 in
+        b )
+            shift
+            dotnet build "$@"
+        ;;
+        r )
+            shift
+            dotnet run "$@"
+        ;;
+        * )
+            dotnet "$@"
+        ;;
+    esac
+}
+# golang cli commands
+function gl() {
+    case $1 in
+            b )
+            shift
+            go build .
+        ;;
+        r )
+            shift
+            go run .
+        ;;
+        * )
+            go "$@"
+        ;;
+    esac
+}
+# cargo cli commands
+function cg() {
+    case $1 in
+        b )
+            shift
+            cargo build
+        ;;
+        c )
+            shift
+            cargo check
+        ;;
+        r )
+            shift
+            cargo run
+        ;;
+        * )
+            cargo "$@"
+        ;;
+    esac
+}
